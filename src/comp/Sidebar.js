399e5c16b164942b017a13618db0a233f5165167
yashk21 <yashk8119@gmail.com>
2022-09-08 02:04:00 +0530

    comp created

---
+++ src/comp/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from '../comp/Nav'
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <Nav/>
+    </div>
+  )
+}
+
+export default Sidebar
