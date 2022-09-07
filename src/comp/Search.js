4b94551c6aca2f686d4e122e9c452df95111a674
yashk21 <yashk8119@gmail.com>
2022-09-08 03:59:54 +0530

    Styled and Updated

--- src/comp/Search.js
+++ src/comp/Search.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" placeholder='chat with friend' />
+      </div>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmvJ13vfrJEW1xdXvIBx2Wt-Z48ZF7EfQ7Cmz068c&s" alt="" />
+        <div className="userChatInfo">
+          <span>Temp</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
