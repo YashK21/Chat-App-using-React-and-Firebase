686cbb08fd5d05fb277aeb94c5919a756a6d2d16
yashk21 <yashk8119@gmail.com>
2022-09-08 04:01:14 +0530

    Styled and Updated -Nav bar

--- src/comp/Nav.js
+++ src/comp/Nav.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
 const Nav = () => {
+  const click = () => {
+    alert("clicked")
+  }
   return (
-    <div>Nav</div>
+    <div className='nav'>
+      <span className='logo'>Chat App
+      </span>
+        <div className='user'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmvJ13vfrJEW1xdXvIBx2Wt-Z48ZF7EfQ7Cmz068c&s" alt='' onClick={click} />
+          <span>Yash</span>
+          <button>Logout</button>
+        </div>
+    </div>
   )
 }
 
