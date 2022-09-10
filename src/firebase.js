7f8d71b43db6012ad945376450bd0de134f2d1a9
yashk21 <yashk8119@gmail.com>
2022-09-11 03:36:08 +0530

    Firebase fun updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
-import "../config.json"
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import {getStorage} from "firebase/storage"
 const firebaseConfig = {
-  apiKey: config.apiKey,
+  apiKey: "AIzaSyBAt7uE3vJRgjO8-MII7qxTBkcah-FWjew",
   authDomain: "chat-app-using-react-12414.firebaseapp.com",
   projectId: "chat-app-using-react-12414",
   storageBucket: "chat-app-using-react-12414.appspot.com",
-  messagingSenderId: config.messagingSenderId,
-  appId: "1:803344431152:web:3d602e2e3f696ee32595f5"
+    messagingSenderId: "803344431152",
+  appId: "1:803344431152:web:3d602e2e3f696ee32595f5",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth()
+export const auth = getAuth();
+export const storage = getStorage()
+export const db = getFirestore()
