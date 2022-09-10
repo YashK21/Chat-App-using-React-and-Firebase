a7350b4b1f92240e4a84203619ae43e5a1fb252c
yashk21 <yashk8119@gmail.com>
2022-09-11 01:16:38 +0530

    Updated UI

--- src/comp/Message.js
+++ src/comp/Message.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmvJ13vfrJEW1xdXvIBx2Wt-Z48ZF7EfQ7Cmz068c&s" alt='' />
+      {/* to show message time */}
+      <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmvJ13vfrJEW1xdXvIBx2Wt-Z48ZF7EfQ7Cmz068c&s" alt='' />
+      </div>
+    </div>
   )
 }
 
